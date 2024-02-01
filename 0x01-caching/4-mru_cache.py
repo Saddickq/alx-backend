5349a58481e20d40d97ab188bd32678de4df038a
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-02-01 10:02:34 +0000

    most recently used

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""FIFOCache module"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """LRUCache class"""
+    def __init__(self):
+        """init method"""
+        self.usedKeys = []
+        super().__init__()
+
+    def put(self, key, item):
+        """put method"""
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                del self.cache_data[self.usedKeys[-1]]
+                print(f"DISCARD: {self.usedKeys[-1]}")
+                self.usedKeys.pop(-1)
+            self.cache_data[key] = item
+            self.usedKeys.append(key)
+
+    def get(self, key):
+        """get method"""
+        if key and self.cache_data.get(key):
+            self.usedKeys.remove(key)
+            self.usedKeys.append(key)
+            return self.cache_data.get(key)
